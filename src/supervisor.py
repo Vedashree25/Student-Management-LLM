371120e53cfda0071a58d609e81c182cbcd3f597
Vedashree25 <140238993+Vedashree25@users.noreply.github.com>
2024-09-03 11:21:16 +0530

    Update supervisor.py

--- src/supervisor.py
+++ src/supervisor.py
@@ -1,9 +1,8 @@
-# src/supervisor.py
 class Supervisor:
     def __init__(self, adapter):
         self.adapter = adapter
 
-    def handle_query(self, user_query):
+    def query(self, user_query):
         context = {}
         response = self.adapter.process_query(user_query, context)
         return response
