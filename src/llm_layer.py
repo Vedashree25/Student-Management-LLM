707f71c4466784347c3258eafeb943de9b166ea6
Vedashree25 <140238993+Vedashree25@users.noreply.github.com>
2024-09-03 11:29:38 +0530

    Update llm_layer.py

--- src/llm_layer.py
+++ src/llm_layer.py
@@ -1,10 +1,8 @@
-
 from transformers import pipeline
 
 class LLMLayer:
     def __init__(self, model_name):
-        self.model = pipeline('text-generation', model="gpt2")  # or another valid model name
-
+        self.model = pipeline('text-generation', model="gpt2") 
 
     def get_response(self, query, context):
         response = self.model(query, max_length=100)[0]['generated_text']
